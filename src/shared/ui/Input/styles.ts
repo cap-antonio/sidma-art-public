85e677c906554e4345e59be9165b5cdbc6f1e94b
Viktor Hashenko <hashenko.cz@gmail.com>
2023-03-08 22:24:19 +0100

    Input

---
+++ src/shared/ui/Input/styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const StyledInput = styled.input`
+  display: flex;
+  padding: 5px;
+  margin: 10px;
+  height: 40px;
+  &::placeholder {
+    color: black;
+  }
+`
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react'
-
 import { StyledHeader } from './styles'
 
 export const Header: FC = () => {
