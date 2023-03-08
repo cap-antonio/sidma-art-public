f70c7d00dc84bca145e80d647ec2a4c5b01b7064
Anton Cheremnykh <a.cheremnykh@idynsys.org>
2023-03-08 22:09:44 +0100

    fix button

--- src/shared/ui/Button/styles.ts
+++ src/shared/ui/Button/styles.ts
@@ -1,14 +1,15 @@
 import styled from '@emotion/styled'
 
 export const StyledButton = styled.button`
-  background-color: white;
+  background: white;
   color: black;
   padding: 8px;
   margin: 48px;
   cursor: pointer;
   transition: 0.35s;
+
   &:hover {
-    background-color: black;
+    background: black;
     color: white;
   }
 `
