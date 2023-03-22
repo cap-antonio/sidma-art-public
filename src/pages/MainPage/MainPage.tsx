5c5a616168d117d921cb8d93ff4922a2f869b3e6
dmitriinaletov <dmitriinaletov@gmail.com>
2023-03-22 15:51:37 +0100

    Routes Main Page and Shop

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
 
 export const MainPage: FC = () => {
-  return <></>
+  return <div>Main Page</div>
 }
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,7 +1,23 @@
 import { FC } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
-import { StyledHeader } from './styles'
+import { MainPage } from 'src/pages'
+import { Shop } from 'src/pages/Shop'
 
 export const Header: FC = () => {
-  return <StyledHeader>header</StyledHeader>
+  return (
+    <>
+      <a href="/">
+        <MainPage />
+      </a>
+      <a href="/shop">
+        <Shop />
+      </a>
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/shop" element={'Купи, заебал'} />
+      </Routes>
+    </>
+  )
 }
