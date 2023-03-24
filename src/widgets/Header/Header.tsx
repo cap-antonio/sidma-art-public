6fcd7eea3171329a5d4c749f0c87a83472a641dc
Viktor Hashenko <hashenko.cz@gmail.com>
2023-03-24 14:03:00 +0100

    Input without Header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,26 +1,6 @@
-import { ChangeEvent, FC, useState } from 'react'
-import { Input, Button } from '../../shared/ui'
+import { FC } from 'react'
 import { StyledHeader } from './styles'
 
 export const Header: FC = () => {
-  const [name, SetName] = useState<string>('')
-  const onNameChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
-    SetName(target.value)
-  }
-
-  //{e}:ChangeEvent<HTMLInputElement>
-
-  return (
-    <StyledHeader>
-      <Input
-        variant="primary"
-        placeholder="Name*"
-        value={name}
-        onChange={onNameChange}
-      />
-      <Input variant="secondary" placeholder="Email*" />
-
-      <Button title="Push" />
-    </StyledHeader>
-  )
+  return <StyledHeader></StyledHeader>
 }
