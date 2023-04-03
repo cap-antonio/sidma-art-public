9f76bce66f263f82d38e95a3b4487a2d88808fda
Viktor Hashenko <hashenko.cz@gmail.com>
2023-04-03 18:24:52 +0200

    IconButton

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,12 +1,6 @@
 import { FC } from 'react'
 import { StyledHeader } from './styles'
-import { IconButton } from '../../shared/ui/IconButton/IconButton'
 
 export const Header: FC = () => {
-  return (
-    <StyledHeader>
-      <IconButton variant={'primary'} />
-      <IconButton variant={'secondary'} />
-    </StyledHeader>
-  )
+  return <StyledHeader></StyledHeader>
 }
