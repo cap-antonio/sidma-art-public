ef6a8f6128a47d315d71aeea8c0a8d7783c3f4fc
Viktor Hashenko <hashenko.cz@gmail.com>
2023-06-05 20:00:02 +0200

    adaptive Contact Page and deleted box-sizing

--- src/pages/ContactsPage/styles.ts
+++ src/pages/ContactsPage/styles.ts
@@ -7,12 +7,10 @@ export const FormWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  box-sizing: border-box;
 
   width: 100%;
   margin: 8px auto;
   max-width: 600px;
-  padding: 16px;
   ${media.md} {
     margin: 16px;
   }
