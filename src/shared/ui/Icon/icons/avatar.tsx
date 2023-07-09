174e0e5d674a29a0b09c4de4e67fb694ecc902e9
dmitriinaletov <dmitriinaletov@gmail.com>
2023-07-09 13:12:12 +0200

    tag image for all of icons

--- src/shared/ui/Icon/icons/avatar.tsx
+++ src/shared/ui/Icon/icons/avatar.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 export const Avatar: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    height="18"
-    viewBox="0 0 512 512"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <circle fill="current" cx="256" cy="114.526" r="114.526" />
-    <path
-      fill="current"
-      d="M256,256c-111.619,0-202.105,90.487-202.105,202.105c0,29.765,24.13,53.895,53.895,53.895h296.421
+  <image>
+    <svg
+      height="18"
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle fill="current" cx="256" cy="114.526" r="114.526" />
+      <path
+        fill="current"
+        d="M256,256c-111.619,0-202.105,90.487-202.105,202.105c0,29.765,24.13,53.895,53.895,53.895h296.421
 			c29.765,0,53.895-24.13,53.895-53.895C458.105,346.487,367.619,256,256,256z"
-    />
-  </svg>
+      />
+    </svg>
+  </image>
 )
