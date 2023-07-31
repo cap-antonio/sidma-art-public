a8337ba376f0abcc8655229e5e1acb0c0c8411f6
dmitriinaletov <dmitriinaletov@gmail.com>
2023-07-31 10:31:15 +0200

    fix_icons

--- src/shared/ui/Icon/icons/close.tsx
+++ src/shared/ui/Icon/icons/close.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 
 export const Close: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <image href="data..">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-      width="22px"
-      viewBox="0 0 125 125"
-    >
+  <picture>
+    <svg {...props} width="22px" viewBox="0 0 125 125">
       <polygon points="96,14 82,0 48,34 14,0 0,14 34,48 0,82 14,96 48,62 82,96 96,82 62,48 " />
     </svg>
-  </image>
+  </picture>
 )
